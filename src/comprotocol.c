4a68a91beba4e84b23938b51ca71ae63c89dec62
Lucien Zuercher(PM) <l.zuercher@tincan.ch>
2014-07-02 23:24:23 +0200

    project setup

---
+++ src/comprotocol.c
@@ -0,0 +1,17 @@
+/*
+ * comprotocol.c
+ *
+ * Created: 2/07/2014 11:11:58 PM
+ *  Author: Lucien
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
