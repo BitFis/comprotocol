8bb0dc2880bac07406dceb7ed053dc48f121e478
Lucien Zürcher <lucien.zuercher@hotmail.com>
2014-09-03 11:06:12 +0200

    Create co.write
    Contains all functions and helpers used to write on to the connection

---
+++ src/co_write.h
@@ -0,0 +1,46 @@
+/****************************************************************************
+* Copyright (C) 2012 by Emanuel Foster & Lucien Zuercher *
+* *
+* Dieses Dokument ist Teil von "comprotocol" *
+* *
+* Comprotocol ist eine frei zu nutzende Bibliothek. Die Bibliothek wird *
+* unter der MIT Lizens veröffentlicht. Jegliche Nutzung auf eigene Gefahr. *
+****************************************************************************/
+/**
+* @file co_write.h
+* @author Emanuel Foster, Lucien Zuercher
+* @date 3. Sep. 2014
+* @brief Enthaelt Funktionen zum Absenden einer Nachricht
+*
+* Dieses Dokument enthaelt Funktionen und Hilfsfunktionen um eine Nachricht
+* nach dem <i>comprotocol</i> Protokoll abzusenden. Die Bits werden 
+* Interruptweise nach einem definiertem Timer übertragen.
+*/
+
+#ifndef _CO_WRITE_H
+# define _CO_WRITE_H
+
+/****************************************************************************
+* Includes
+****************************************************************************/
+
+# include "co.h"
+
+/****************************************************************************
+* Makros
+****************************************************************************/
+
+
+/****************************************************************************
+* Enumirationen & Strukturen
+****************************************************************************/
+
+
+/****************************************************************************
+* Funktionen
+****************************************************************************/
+
+
+#endif /* _CO_WRITE_H */
+
+/****************************************************************************/
